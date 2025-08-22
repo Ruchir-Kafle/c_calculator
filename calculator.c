da66801c76a034d33d80c2128b76ecac93842be1
ThatGuyRuchir <150929311+Ruchir-Kafle@users.noreply.github.com>
2025-08-21 22:23:34 -0400

    upload

---
+++ calculator.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define FIRST_STAGE "Input first number: "
+#define SECOND_STAGE "Input operation: "
+#define THIRD_STAGE "Input second number: "
+#define STAGES 3
+#define MAX_CHARACTERS 22
+
+void print(char message[]) {
+    for (int i; message[i] != '\0'; i++) {
+        putchar(message[i]);
+    }
+}
+
+int is_digit(int a) {
+    if (a >= '0' && a <= '9') {
+        return 1;
+    }
+
+    return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    return a / b;
+}
+
+int main() {
+
+    int c;
+
+    int stage = 0;
+    int count = 0;
+
+    long int first_int = 0;
+    int operation;
+    long int second_int = 0;
+    
+    print("FIRST_STAGE");
+    
+    while ((c = getchar()) != EOF) {
+        
+        if (stage == 0) {
+            if (is_digit(c)) {
+                first_int = first_int * 10 + (c - '0');
+            }
+        } else if (stage == 1) {
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
+                operation = c;
+            }
+        } else if (stage == 2) {
+            if (is_digit(c)) {
+                second_int = second_int * 10 + (c - '0');
+            }
+        } else {continue;}
+
+        putchar(c);
+
+        if (c == '\n') {
+            stage++;
+
+            if (stage == 3) {
+                int result;
+
+                if (operation == '+') {result = add(first_int, second_int);}
+                if (operation == '-') {result = sub(first_int, second_int);}
+                if (operation == '*') {result = multiply(first_int, second_int);}
+                if (operation == '/') {result = divide(first_int, second_int);}
+
+                printf("Result: %i", result);
+
+                break;
+            } else {
+                char stages[STAGES][MAX_CHARACTERS] = {FIRST_STAGE, SECOND_STAGE, THIRD_STAGE};
+
+                print(stages[stage]);
+            }
+        }        
+    }
+
+    return 0;
+}
